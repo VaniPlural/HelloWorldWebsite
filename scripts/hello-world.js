363cfe0e99ff731f457b1955b5f4d340c753a1c5
VaniPlural <vani-hosagoudra@pluralsight.com>
2024-05-07 21:23:42 +0530

    HElloworldwebsite of javascript

---
+++ scripts/hello-world.js
@@ -0,0 +1,23 @@
+
+"use strict";
+
+// A function to show a greeting
+function showGreeting() {
+   // TODO: Find the messageDiv element
+
+   // TODO: Set the div's contents to a string
+   let messageDiv = document.getElementById("messageDiv");
+   messageDiv.innerHTML = "Hello World!";
+let nameField = document.getElementById("nameField");
+let clickMeBtn = document.getElementById("clickMeBtn");
+
+}       
+
+// calls the showGreeting() function when the .js
+// file loads and the line below is encountered
+showGreeting();
+
+
+
+
+
